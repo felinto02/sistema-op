29bf13995454ddfae2de14df9b4d5c59079dc079
felinto02 <117090962+felinto02@users.noreply.github.com>
2026-03-06 09:14:47 -0400

    Update db.js

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,11 +1,11 @@
+const { setDefaultResultOrder } = require('dns');
+setDefaultResultOrder('ipv4first');
+
 const { Pool } = require('pg');
 require('dotenv').config();
 
-// Verifica se existe a URL do Render, caso contrário, monta a conexão local
 const isProduction = process.env.DATABASE_URL && process.env.DATABASE_URL.includes('render.com');
-
 const pool = new Pool({
-    // Se tiver DATABASE_URL (Render), usa ela. Se não, usa os campos separados (Local).
     connectionString: process.env.DATABASE_URL || `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`,
     ssl: isProduction ? { rejectUnauthorized: false } : false
 });
